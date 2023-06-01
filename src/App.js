bdf11d4b975a1785da2780e2648d5e77ec12644b
rigueto <pabloedurigueto@outlook.com>
2023-05-31 22:38:52 -0300

    Clonned ok.

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ function App() {
   const [qrCodeValue, setQRCodeValue] = useState("");
   const [shouldPrint, setShouldPrint] = useState(false);
   const [printSize, setPrintSize] = useState("10cm 5cm");
-
-  const printRef = useRef(null);
+  const [duplicateCount, setDuplicateCount] = useState(1);
 
   const generateQRCodeData = () => {
     const texto_livre = `${dataOperacao}\t  ${pedido}\t ${cliente}\t  ${loja}\t  ${volume}`;
@@ -23,23 +22,8 @@ function App() {
 
   useEffect(() => {
     if (qrCodeValue !== '' && shouldPrint) {
-      const printContent = printRef.current;
-      console.log(printContent);
-
-      const printDocument = document.createElement('div');
-      printDocument.innerHTML = `
-        <div class="print-copy">${printContent.innerHTML}</div>
-        <div class="print-copy">${printContent.innerHTML}</div>
-        <div class="print-copy">${printContent.innerHTML}</div>
-      `;
-
-      document.body.appendChild(printDocument);
-
       window.print();
       setShouldPrint(false);
-
-      // Clean up the dynamically added print document
-      document.body.removeChild(printDocument);
     }
   }, [qrCodeValue, shouldPrint]);
 
@@ -53,6 +37,7 @@ function App() {
     event.preventDefault();
     setShouldPrint(true);
     updateQrCode(event);
+    setDuplicateCount(parseInt(volume, 10));
   };
 
   useEffect(() => {
@@ -79,100 +64,64 @@ function App() {
 
   return (
     <>
-      <div className="main__content">
-        <div className="printSizeParent">
-          {/* Logotipo */}
-          <button className="logotipo"></button>
-
-          {/* Title field */}
-          <div className="title printTitle">
-            <h2 className="title__h2">ETIQUETA DE ESTOQUE</h2>
-          </div>
-
-          {/* Change print size button */}
-          <button className="printSize" onClick={handlePrintSizeChange}>
-            <MdSettings />
-          </button>
-        </div>
-
-        {/* Parent fields and QR Code */}
-        <div className="parent">
-          {/* Form fields */}
-          <div className="fields">
-            <EtiquetaForm
-              handleSubmit={handleSubmit}
-              dataOperacao={dataOperacao}
-              setDataOperacao={setDataOperacao}
-              pedido={pedido}
-              setPedido={setPedido}
-              cliente={cliente}
-              setCliente={setCliente}
-              loja={loja}
-              setLoja={setLoja}
-              volume={volume}
-              setVolume={setVolume}
-            />
-          </div>
-
-          {/* QR Code Field */}
-          <div className="qrcode__field">
-            <QRCode key={qrCodeValue} value={qrCodeValue} className="qrcode" />
-          </div>
-        </div>
-      </div>
-
-      <div className="buttons">
-        {/* Print button */}
-        <div className="printParent">
-          <div className="printButton">
-            <button className="print__btn" onClick={handleSubmit}>
-              <MdPrint /> Print
-            </button>
-          </div>
-        </div>
-      </div>
-
-      {/* Hidden print content */}
-      <div style={{ display: 'none' }}>
-        <div ref={printRef}>
-          {/* Content to be printed */}
+    <div>
+      {Array.from({ length: duplicateCount }, (_, index) => (
+        <React.Fragment key={index}>
           <div className="main__content">
             <div className="printSizeParent">
+              {/* Logotipo */}
               <button className="logotipo"></button>
+
+              {/* Title field */}
               <div className="title printTitle">
                 <h2 className="title__h2">ETIQUETA DE ESTOQUE</h2>
               </div>
+
+              {/* Change print size button */}
               <button className="printSize" onClick={handlePrintSizeChange}>
                 <MdSettings />
               </button>
             </div>
+
             <div className="parent">
-              <div className="fields">
-              {console.log(qrCodeValue)}
-                <EtiquetaForm
-                  dataOperacao={dataOperacao}
-                  setDataOperacao={setDataOperacao}
-                  pedido={pedido}
-                  setPedido={setPedido}
-                  cliente={cliente}
-                  setCliente={setCliente}
-                  loja={loja}
-                  setLoja={setLoja}
-                  volume={volume}
-                  setVolume={setVolume}
-                />
-              </div>
+              {/* Form fields */}
+                <div className="fields">
+                  <EtiquetaForm
+                    handleSubmit={handleSubmit}
+                    dataOperacao={dataOperacao}
+                    setDataOperacao={setDataOperacao}
+                    pedido={pedido}
+                    setPedido={setPedido}
+                    cliente={cliente}
+                    setCliente={setCliente}
+                    loja={loja}
+                    setLoja={setLoja}
+                    volume={volume}
+                    setVolume={setVolume}
+                  />
+                </div>
 
-              {/* Generate new qrcode here */}
-              <div className="qrcode__field">
-                <QRCode key={qrCodeValue} value={qrCodeValue} className="qrcode" />
-              </div>
+                {/* QR Code Field */}
+                <div className="qrcode__field">
+                  <QRCode key={qrCodeValue} value={qrCodeValue} className="qrcode" />
+                </div>
+            </div>
 
+          </div>
+
+          <div className="buttons">
+            {/* Print button */}
+            <div className="printParent">
+              <div className="printButton">
+                <button className="print__btn" onClick={handleSubmit}>
+                  <MdPrint /> Print
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-
+        </React.Fragment>
+      ))}
+    </div>
     </>
   );
 }
